4ee86dcfde95a7271316707e1aeac15c283d062a
anaclara77-m <gorges.ana@escola.pr.gov.br>
2025-09-03 19:35:34 +0000

    commit

--- aula06.py
+++ aula06.py
@@ -6,12 +6,12 @@ np.random.seed(42)
 
 dados = pd.DataFrame({
     "Orcamento Campanha": np.random.randint(1000, 5000, size=100),
-        "Visualizacoes Anuncio": np.random.randint(2000, 200000, size=100),
-            "Vendas": np.random.randint(10, 10000, size=100),
-                "Cliques": np.random.randint(100, 20000, size=100)
-                })
+    "Visualizacoes Anuncio": np.random.randint(2000, 200000, size=100),
+    "Vendas": np.random.randint(10, 10000, size=100),
+    "Cliques": np.random.randint(100, 20000, size=100)
+})
 
-                print(dados.describe())
+print(dados.describe())
 
-                # Visualizar a correlação entre cada variável
-                print(dados.corr().round(4))
+# Visualizar a correlação entre cada variável
+print(dados.corr().round(4))
